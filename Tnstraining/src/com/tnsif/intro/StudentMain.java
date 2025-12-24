e6ea4576d2517a528c721b33870573c3356bf8d5
LAPTOP <LAPTOP@hp>
2025-12-24 20:31:30 +0530

    encap

---
+++ Tnstraining/src/com/tnsif/intro/StudentMain.java
@@ -0,0 +1,16 @@
+package com.tnsif.intro;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		
+		        Student s = new Student();
+
+		        s.setId(101);
+		        s.setName("Ashwitha");
+
+		        System.out.println("ID: " + s.getId());
+		        System.out.println("Name: " + s.getName());
+		    }
+		}
+	
