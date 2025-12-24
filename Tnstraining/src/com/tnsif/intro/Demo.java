b5308e26603fd20a22c8c41f372bb1bb88c93423
LAPTOP <LAPTOP@hp>
2025-12-24 20:03:54 +0530

    commit

--- Tnstraining/src/com/tnsif/intro/Demo.java
+++ Tnstraining/src/com/tnsif/intro/Demo.java
@@ -1,12 +1,15 @@
 package com.tnsif.intro;
+public class Demo{
+	public static void main (String args[]) {
+		System.out.println("Hello world");
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
 
-public class Demo {
 
-	public static void main(String[] args) {
-		System.out.print("Hello World");
-int a=10;
-int b=20;
-System.out.println(a+b);
 	}
-
 }
