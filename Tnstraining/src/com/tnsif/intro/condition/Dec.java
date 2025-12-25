0092870d83f41962a9a4353fff8fea957718df6f
LAPTOP <LAPTOP@hp>
2025-12-25 20:51:56 +0530

    conditional statements

--- Tnstraining/src/com/tnsif/intro/condition/Dec.java
+++ Tnstraining/src/com/tnsif/intro/condition/Dec.java
@@ -6,19 +6,9 @@ public class Dec {
 		int n=2;
 		if(n==2) {
 			System.out.println("True");
-		}
-		int a=30;
-		if(a%2==0) {
-			System.out.println("Even");
-		}
-		else {
-		System.out.println("Odd");
-
 			}
+	}
 		
 
-		}
-
-	}
-
+}
 
