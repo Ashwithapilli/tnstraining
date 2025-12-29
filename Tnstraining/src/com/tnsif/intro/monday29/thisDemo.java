0e26670c62e15f4c4c9c4583e33860dadc520501
LAPTOP <LAPTOP@hp>
2025-12-29 20:03:22 +0530

    this keyword

---
+++ Tnstraining/src/com/tnsif/intro/monday29/thisDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.intro.monday29;
+
+public class thisDemo {
+
+	public static void main(String[] args) {
+		thiss obje=new thiss();
+		obje.show();
+
+	}
+
+}
---
+++ Tnstraining/src/com/tnsif/intro/monday29/thiss.java
@@ -0,0 +1,11 @@
+package com.tnsif.intro.monday29;
+
+public class thiss {
+	int a=10;
+	public void show() {
+		int a=39;
+		System.out.println(a);
+		System.out.println(this.a);
+	}
+
+}
