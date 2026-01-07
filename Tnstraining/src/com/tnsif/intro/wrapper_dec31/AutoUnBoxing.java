9ae193380104ad7d408b1179933fd8b95d248eae
LAPTOP <LAPTOP@hp>
2026-01-07 21:55:45 +0530

    Wrapperclass

---
+++ Tnstraining/src/com/tnsif/intro/wrapper_dec31/AutoUnBoxing.java
@@ -0,0 +1,11 @@
+package com.tnsif.intro.wrapper_dec31;
+
+public class AutoUnBoxing {
+
+	public static void main(String[] args) {
+		Double b=23.4567;
+		double g=b;
+		System.out.println(g);
+	}
+
+}
