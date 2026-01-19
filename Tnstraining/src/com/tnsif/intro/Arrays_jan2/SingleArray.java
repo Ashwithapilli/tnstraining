fee7ab60ade81ca4cc845455f121c678e1c3811a
LAPTOP <LAPTOP@hp>
2026-01-19 18:17:48 +0530

    Arrays

---
+++ Tnstraining/src/com/tnsif/intro/Arrays_jan2/SingleArray.java
@@ -0,0 +1,18 @@
+package com.tnsif.intro.Arrays_jan2;
+
+public class SingleArray {
+
+	public static void main(String[] args) {
+		int[] arr= {1,2,3,4,5};
+		System.out.println(arr[2]);
+		//System.out.println(arr[6]); // raises ArrayIndexOutOfBound Exception
+
+		int[] num=new int[3];
+		num[0]=45;
+		num[1]=5;
+		num[2]=57;
+		for(int i=0;i<3;i++) {
+			System.out.println(num[i]);	
+		}
+}
+}
